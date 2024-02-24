b8c89e31e0d481d0da9a99c7e0e4771555e8b407
shobana-anand <117923399+shobana-anand@users.noreply.github.com>
2024-02-24 06:24:05 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+import logging
+import os
+#from dotenv import load_dotenv  # Import load_dotenv from dotenv package
+
+# Load environment variables from config.env and secrets.env files
+#load_dotenv("config.env")
+#load_dotenv("secrets.env")
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db_username = os.environ["DB_USERNAME"]
+db_password = os.environ["DB_PASSWORD"]
+db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_port = os.environ.get("DB_PORT", "5432")
+db_name = os.environ.get("DB_NAME", "postgres")
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+db = SQLAlchemy(app)
+
+app.logger.setLevel(logging.DEBUG)
